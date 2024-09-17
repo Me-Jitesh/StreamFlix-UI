1a29bebf7916f313f690e9cdbb773cffc1ea3a5b
Jitesh Singh <jiteshs101@gmail.com>
2024-09-17 12:46:13 +0530

    Tailwind CSS Configured

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/**/**/*.{html,ts}',  // Adjust according to your Angular files
+    './src/**/*.{html,ts}',  // Adjust according to your Angular files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
